41f232ddce583ee9bac5491b382fb903798cf49e
raygarza <raygarza361@gmail.com>
2020-06-04 21:27:54 -0500

    Updates

---
+++ precache-manifest.669e64461372e645b6def8a622b5f6e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc95a75983049cfe21098d63eec651bf",
+    "url": "/react_step_form/index.html"
+  },
+  {
+    "revision": "576433e20863b7403a60",
+    "url": "/react_step_form/static/css/main.d6937961.chunk.css"
+  },
+  {
+    "revision": "48463e7b21966c7ead74",
+    "url": "/react_step_form/static/js/2.71cb35fd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react_step_form/static/js/2.71cb35fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "576433e20863b7403a60",
+    "url": "/react_step_form/static/js/main.a126cb3e.chunk.js"
+  },
+  {
+    "revision": "a16cac3d60ac45d48e21",
+    "url": "/react_step_form/static/js/runtime-main.58bd7da6.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react_step_form/precache-manifest.669e64461372e645b6def8a622b5f6e0.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_step_form/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
